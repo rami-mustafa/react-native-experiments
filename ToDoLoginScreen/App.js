86f9ba6bb67c05534c0e860303ab2c226bfe8c85
rami-mustafa <ramimustafa77@gmail.com>
2021-10-25 14:00:05 +0300

    update app.js

--- ToDoLoginScreen/App.js
+++ ToDoLoginScreen/App.js
@@ -1,12 +1,28 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './src/screens/HomeScreen/Home';
+import Login from './src/screens/LoginScreen/Login';
+import Register from './src/screens/RegisterScreen/Register';
 
-const App = () => {
+const Stack = createNativeStackNavigator();
+
+function App() {
   return (
-    <View>
-      <Text>adsafdaf</Text>
-    </View>
-  )
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home}
+          options={{ headerShown: false }} />
+        <Stack.Screen name="Login" component={Login}
+          options={{ headerShown: false }} />
+        <Stack.Screen name="Register" component={Register}
+          options={{ headerShown: false }} />
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
-export default App
+export default App;
+
